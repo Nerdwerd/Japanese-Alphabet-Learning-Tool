a3b34cba2972f489ff5d57800dadad626dce95ef
Nerdwerd <50635875+Nerdwerd@users.noreply.github.com>
2020-09-22 17:21:02 -0500

    Testing Window Size

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 
 public class Main {
 	
+	public static int WIDTH = 500, HEIGHT = 600;
+	
 	public static void main(String[]args) {
 		
 		Startup();
@@ -16,7 +18,7 @@ public class Main {
 		JPanel panel = new JPanel();
 		panel.setLayout(new FlowLayout());
 		window.add(panel);
-		window.setSize(1000,1000);
+		window.setSize(WIDTH,HEIGHT);
 		window.setLocationRelativeTo(null);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
